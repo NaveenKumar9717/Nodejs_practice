fd7ec3cc1f0a5e933fcb14165184714e5a09b4b3
Naveen Kumar <Naveencruz20@gmail.com>
2021-03-14 10:24:21 +0530

    SERVER CLIEN TUSAGE WITHOUT EXPRESS

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
-const people = ['naveen', 'kapil', 'vikas']
-const ages = [3,4,5,6] 
-console.log(people)
+const http = require("http");
+const fs = require("fs");
+const _  = require('lodash')
 
-module.exports = { people,ages}
+
+
+const server = http.createServer((req, res) => {
+    
+    // lodash 
+    const num = _.random(0,20);
+    console.log(num) 
+
+
+
+    res.setHeader("Content-Type", "text/html");
+  fs.readFile("./views/index.html", (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      //res.write(data)
+      res.end(data);
+    }
+  });
+
+});
+
+server.listen(3000, () => {
+  console.log("Hi connected");
+});
+
